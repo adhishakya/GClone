9a560332e5651d858d0c52c01bdeaf233cf55b2c
Adhish Shakya <shakyaadhish@gmail.com>
2022-11-04 18:13:27 +0545

    Logo for dark mode added + fixed texts in darkmode

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 let darkButton = document.getElementById("darkLight");
+let logo = document.getElementById("logo");
 
 darkButton.onclick = () => {
   let element = document.body;
   element.classList.toggle("darkMode");
   if (darkButton.src.endsWith("/images/moon.png") == true) {
     darkButton.src = "/images/sun.png";
+    logo.src = "/images/googleDark.svg";
   } else if (darkButton.src.endsWith("/images/sun.png") == true) {
     darkButton.src = "/images/moon.png";
+    logo.src = "/images/google.svg";
   }
 };
