49b084a83537ea10bbd4999bd7ad34f9cbb71390
Adhish Shakya <shakyaadhish@gmail.com>
2022-11-06 16:25:42 +0545

    Advanced mode additions

---
+++ js/advanced.js
@@ -0,0 +1,17 @@
+let darkButton = document.getElementById("darkLight");
+let logo = document.getElementById("logo");
+
+darkButton.onclick = () => {
+  let element = document.body;
+  element.classList.toggle("darkMode");
+  //Toggling dark mode
+  if (darkButton.src.endsWith("/images/moon.png") == true) {
+    darkButton.src = "/images/sun.png";
+    logo.src = "/images/googleDark.svg";
+  }
+  //Toggling light mode
+  else if (darkButton.src.endsWith("/images/sun.png") == true) {
+    darkButton.src = "/images/moon.png";
+    logo.src = "/images/google.svg";
+  }
+};
